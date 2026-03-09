52939d8f13f26c80b8c3c8946decefd22dc17fad
David North <bandanna2k@gmail.com>
2026-03-09 17:51:37 +1300

    AI - Build models script update

--- local-llm/docker/build.sh
+++ local-llm/docker/build.sh
@@ -1,1 +1,23 @@
+set -e
+
+OLLAMA_IMAGES=$(docker images --format table |grep ollama-with-models || true)
+
+if [ -n "$OLLAMA_IMAGES" ]; then
+  echo "Image already exists."
+  echo $OLLAMA_IMAGES
+
+  exit 0
+fi
+
+OLLAMA_DISK_IMAGES=$(ls -al |grep ollama-with-models || true)
+if [ -n "$OLLAMA_DISK_IMAGES" ]; then
+  echo "Image exists on disk. Loading ..."
+  echo $OLLAMA_DISK_IMAGES
+  docker load -i ollama-with-models.tar
+  docker images --format table | grep ollama
+  exit 0
+fi
+
 docker build -t ollama-with-models .
+docker save -o ollama-with-models.tar ollama-with-models:latest
+
